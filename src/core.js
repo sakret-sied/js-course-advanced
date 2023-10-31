7babf13060d207be9e7c4a20dfa01f20f6d2902c
SakretSied <sacretsid@gmail.com>
2023-10-31 20:20:22 +0400

    renaming

--- src/core.js
+++ src/core.js
@@ -10,32 +10,32 @@ export default class Core {
       return;
     }
 
-    const select = this.#addSelector();
-    this.#addOptionsToSelector(select);
-    this.#addEventListenerToSelector(select);
+    const selector = this.#addSelector();
+    this.#addOptionsToSelector(selector);
+    this.#addEventListenerToSelector(selector);
   }
 
   #addSelector() {
-    const select = document.createElement('select');
-    select.id = this.selectorId;
-    this.container.insertAdjacentElement('beforeend', select);
-    return select;
+    const selector = document.createElement('select');
+    selector.id = this.selectorId;
+    this.container.insertAdjacentElement('beforeend', selector);
+    return selector;
   }
 
-  #addOptionsToSelector(select) {
+  #addOptionsToSelector(selector) {
     const defaultSelector = this.#getDefaultSelector();
     this.modules.forEach((_, index) => {
       const option = document.createElement('option');
       option.id = option.innerText = index;
       option.selected = index === defaultSelector ? 'selected' : '';
-      select.insertAdjacentElement('beforeend', option);
+      selector.insertAdjacentElement('beforeend', option);
     });
 
     this.modules.get(defaultSelector).execute();
   }
 
-  #addEventListenerToSelector(select) {
-    select.addEventListener('change', (e) => {
+  #addEventListenerToSelector(selector) {
+    selector.addEventListener('change', (e) => {
       console.clear();
       this.#setDefaultSelector(e.target.value);
       this.modules.get(e.target.value).execute();
